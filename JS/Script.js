c0b3ec447f942d8a16a15967796a282f228bd672
Ramya-V <rajaramya559@gmail.com>
2020-06-12 15:53:11 +0530

    [IMP] Navigation :  Header and Navigation fixed
    - Header part is done
    - Navigation part is also fixed

--- JS/Script.js
+++ JS/Script.js
@@ -1,2 +1,25 @@
+/* ==============================================
+|   |   |   |   Navigation                              
+================================================= */
+$(function () {
 
+    //happens at page load
+    showhidenav();
+
+    //happens at windows scroll
+    $(window).scroll(function(){
+        showhidenav()
+    });
+
+    function showhidenav(){
+        if( $(window).scrollTop() > 50){
+            //show white now
+            $("nav").addClass("white-nav-top");
+         }
+         else{
+             //Hide white now
+             $("nav").removeClass("white-nav-top");
+            }
+    }
+});
 
